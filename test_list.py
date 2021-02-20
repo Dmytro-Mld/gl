e85053d75437f5d5ed838ecd0d89ccc53ea0aedb
Dmytro Moldovan <79337586+Dmytro-Mld@users.noreply.github.com>
2021-02-20 11:18:13 +0200

    Update test_list.py

--- test_list.py
+++ test_list.py
@@ -16,9 +16,9 @@ class TestListMethods(unittest.TestCase):
 
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
-
-        def test_dmytro_moldovan_(self):
-        	self.assertEqual(len([]), 0)
+    
+    def test_dmytro_moldovan_(self):
+    	self.assertEqual(len([]), 0)
 
 
 if __name__ == '__main__':
